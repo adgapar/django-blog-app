a6cd895b97c267a29e709fc4d6397b509d466ac3
Adilet Gaparov <adilet@capchase.com>
2025-03-02 23:59:48 +0100

    use class based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 def post_list(request):
     posts = Post.published.all()
